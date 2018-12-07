deafe2bc9ae397a7329567363a1c3fa2d5b9e0b5
Maxme3ernard <maximebernardmax@gmail.com>
2018-12-07 11:40:10 +0100

    Update client et serveur

--- client/main.go
+++ client/main.go
@@ -1,1 +1,32 @@
-package client
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func welcome(serverName string) {
+	fmt.Println("Bienvenue sur " + serverName)
+}
+
+func userin(userName string) {
+	fmt.Println(userName + " a rejoint le chat. Bienvenue à lui!")
+}
+
+func userout(userName string) {
+	fmt.Println(userName + " nous a quitté.")
+}
+
+func analyseMessage(text string) {
+	message := strings.Split(text, "\t")
+	message[1] = strings.TrimRight(message[1], "\r\n")
+	switch message[0] {
+
+	}
+}
